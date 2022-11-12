37b6a0681a98bfbae24cee3e94ec720f4f3ed9d2
radicalwaterbear101 <117006737+radicalwaterbear101@users.noreply.github.com>
2022-11-11 23:16:38 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+export function BakeryItem({ item, addToCart }) {
+    return (
+        <li key={item.name} className="BakeryItem">
+            <h2>
+                {item.name}
+            </h2>
+            <img src={item.image} />
+
+            <div className="BakeryItemDetails">
+                {item.price}
+                <p className="Caption">
+                    {item.description}
+                </p>
+
+                <button onClick={() => addToCart(item.price, item.name)}> Add to Cart</button>
+            </div>
+        </li>
+    )
+}
